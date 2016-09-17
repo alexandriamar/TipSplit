d8aee44ace4e56ead1d6070760b6d6ab3a625a21
Alexandria Mar <alexanwm@usc.edu>
2016-09-16 22:51:34 -0700

    Initial Commit

---
+++ TipSplit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipSplit
+//
+//  Created by Alexandria Mar on 9/16/16.
+//  Copyright © 2016 Alexandria Mar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
